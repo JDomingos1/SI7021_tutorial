9e866b050820d796d16212b9b028733ee4b0ae09
JDomingos1 <100766509+JDomingos1@users.noreply.github.com>
2022-04-06 12:13:44 +0100

    Add files via upload

---
+++ SI7021/SI7021.ino
@@ -0,0 +1,55 @@
+#include <Wire.h>
+
+#define si7021Addr 0x40
+
+void setup(){
+  Wire.begin();
+  Serial.begin(9600);
+  Wire.beginTransmission(si7021Addr);
+  Wire.endTransmission();
+  delay(300);
+}
+
+void loop(){
+  unsigned int data[2];
+
+  Wire.beginTransmission(si7021Addr);
+  Wire.write(0xF5);
+  Wire.endTransmission();
+  delay(500);
+
+  Wire.requestFrom(si7021Addr, 2);
+ 
+  if(Wire.available() == 2){
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+  }
+
+  float humidity  = ((data[0] * 256.0) + data[1]);
+  humidity = ((125 * humidity) / 65536.0) - 6;
+
+  Wire.beginTransmission(si7021Addr);
+
+  Wire.write(0xF3);
+  Wire.endTransmission();
+  delay(500);
+
+  Wire.requestFrom(si7021Addr, 2);
+
+  if(Wire.available() == 2){
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+  }
+
+  float temp  = ((data[0] * 256.0) + data[1]);
+  float celsTemp = ((175.72 * temp) / 65536.0) - 46.85;
+ 
+  Serial.print("Humidity : ");
+  Serial.print(humidity);
+  Serial.println(" % RH");
+  Serial.print("Celsius : ");
+  Serial.print(celsTemp);
+  Serial.println(" C");
+
+  delay(1000);
+}
